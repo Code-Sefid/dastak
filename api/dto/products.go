f8c87f83a84cea4b4b6e5ab43065fd4aaafd62c2
soheil <soheilkhaledabdi@gmail.com>
2023-09-23 19:30:31 +0330

    Update -> better code

--- api/dto/products.go
+++ api/dto/products.go
@@ -3,7 +3,7 @@ package dto
 type CreateProductsRequest struct {
 	Title      string  `json:"title" binding:"required"`
 	Price      float64 `json:"price" binding:"required"`
-	CategoryID int     `json:"categoryId" binding:"required"`
+	CategoryID int     `json:"categoryId"`
 	Inventory  int     `json:"inventory" binding:"required"`
 }
 
@@ -18,7 +18,7 @@ type ProductsResponse struct {
 	ID         int                `json:"id"`
 	Title      string             `json:"title"`
 	Price      float64            `json:"price"`
-	Categories CategoriesResponse `json:"categories"`
+	Categories CategoriesResponse `json:"category"`
 	Inventory  int                `json:"inventory"`
 }
 

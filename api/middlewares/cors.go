f1178b2930c8c389835462cf1363b5445926c11d
soheil <soheilkhaledabdi@gmail.com>
2023-09-23 15:42:15 +0330

    Bug -> fix bug cors error

--- api/middlewares/cors.go
+++ api/middlewares/cors.go
@@ -1,17 +1,23 @@
 package middlewares
 
 import (
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
 )
 
 func Cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
+    return func(c *gin.Context) {
 
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "*")
-		c.Header("Access-Control-Allow-Methods", "*")
-		c.Header("Access-Control-Max-Age", "21600")
-		c.Set("content-type", "application/json")
-		c.Next()
-	}
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Header("Access-Control-Allow-Headers", "*")
+        c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        c.Header("Access-Control-Max-Age", "21600")
+        c.Set("content-type", "application/json")
+        
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+        
+        c.Next()
+    }
 }

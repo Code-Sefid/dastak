0bf3bee2281ea6f2d8525659a7d6a93de664539e
soheil <soheilkhaledabdi@gmail.com>
2023-09-26 19:25:52 +0330

    Update -> write better validation for auth

--- api/dto/auth.go
+++ api/dto/auth.go
@@ -1,17 +1,20 @@
 package dto
 
+// Register struct
 type Register struct {
-	FullName    string `json:"full_name" binding:"required,min=3"`
-	AccountType int    `json:"account_type" binding:"required"`
-	SaleCount   uint   `json:"sale_count" binding:"required"`
-	Mobile      string `json:"mobile" binding:"required,min=11,max=11"`
+	FullName    string `json:"full_name" binding:"required,min=3" `
+	AccountType int    `json:"account_type" binding:"required" `
+	SaleCount   uint   `json:"sale_count" binding:"required" `
+	Mobile      string `json:"mobile" binding:"required,min=11,max=11" `
 }
 
+// Login struct
 type Login struct {
-	Mobile   string `json:"mobile" binding:"required,min=11,max=11"`
-	Password string `json:"password" binding:"required,min=4"`
+	Mobile   string `json:"mobile" binding:"required,min=11,max=11" `
+	Password string `json:"password" binding:"required,min=4" `
 }
 
+// Mobile struct
 type Mobile struct {
-	Mobile string `json:"mobile" binding:"required,min=11,max=11"`
+	Mobile string `json:"mobile" binding:"required,min=11,max=11" `
 }

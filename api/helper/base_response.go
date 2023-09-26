f58f6f140a0e54deb48545ec909e11dc2404c234
soheil <soheilkhaledabdi@gmail.com>
2023-09-26 19:55:17 +0330

    Update -> edit response and  custom validation

--- api/helper/base_response.go
+++ api/helper/base_response.go
@@ -1,77 +1,57 @@
 package helper
 
 import (
-	"strings"
-
 	"github.com/soheilkhaledabdi/dastak/api/validation"
 )
 
-type AlertMessage struct {
-	Title   string `json:"title"`
-	Message string `json:"message"`
-}
-
 type BaseHttpResponse struct {
-	Result           any                           `json:"result"`
-	Status           bool                          `json:"status"`
-	Alert            *AlertMessage                 `json:"alert"`
-	ValidationErrors *[]validation.ValidationError `json:"validationErrors"`
-	Error            any                           `json:"error"`
+	Result           any       `json:"result"`
+	Status           bool      `json:"status"`
+	Alert            *string   `json:"alert"`
+	ValidationErrors []*string `json:"validationErrors"`
 }
 
-func GenerateAlert(title, message string) *AlertMessage {
-	if title == "" && message == "" {
+func GenerateAlert(message string) *string {
+	if message == "" {
 		return nil
 	}
-	return &AlertMessage{
-		Title:   title,
-		Message: message,
-	}
+	return &message
 }
 
-func GenerateBaseResponse(result any, Status bool, Title string, Message string) *BaseHttpResponse {
+func GenerateBaseResponse(result any, Status bool, message string) *BaseHttpResponse {
 	return &BaseHttpResponse{
 		Result: result,
 		Status: Status,
-		Alert:  GenerateAlert(Title, Message),
+		Alert:  GenerateAlert(message),
 	}
 }
 
-func GenerateBaseResponseWithError(Status bool, err any, Title string, Message string) *BaseHttpResponse {
+func GenerateBaseResponseWithError(Status bool, err any, message string) *BaseHttpResponse {
 	return &BaseHttpResponse{
 		Status: Status,
-		Error:  err,
-		Alert:  GenerateAlert(Title, Message),
+		Alert:  GenerateAlert(message),
 	}
 }
 
-func GenerateBaseResponseWithAnyError(Status bool, err any, Title string, Message string) *BaseHttpResponse {
+func GenerateBaseResponseWithAnyError(Status bool, err any, message string) *BaseHttpResponse {
 	return &BaseHttpResponse{
-		Alert:  GenerateAlert(Title, Message),
+		Alert:  GenerateAlert(message),
 		Status: Status,
-		Error:  err,
 	}
 }
 
-func GenerateBaseResponseWithValidationError(Status bool, err error, Title string, Message string) *BaseHttpResponse {
+func GenerateBaseResponseWithValidationError(Status bool, err error, message string) *BaseHttpResponse {
 	validationErrs := validation.GetValidationErrors(err)
 	if validationErrs != nil {
-		errorMessages := make([]string, 0)
-		for _, validationErr := range *validationErrs {
-			errorMessage := strings.ToLower(validationErr.Property)
-			errorMessages = append(errorMessages, errorMessage)
-		}
-		titleMessage := "Error"
-		alertMessage := "Please fill " + strings.Join(errorMessages, " , ") + " field correctly"
 		return &BaseHttpResponse{
-			Alert:            GenerateAlert(titleMessage, alertMessage),
+			Alert:            GenerateAlert(message),
 			Status:           Status,
 			ValidationErrors: validationErrs,
 		}
 	}
 
 	return &BaseHttpResponse{
-		Alert:  GenerateAlert(Title, Message),
+		Alert:  GenerateAlert(message),
 		Status: Status,
 	}
 }
--- api/validation/custom.go
+++ api/validation/custom.go
@@ -2,29 +2,52 @@ package validation
 
 import (
 	"errors"
-
 	"github.com/go-playground/validator/v10"
 )
 
 type ValidationError struct {
-	Property string `json:"property"`
-	Tag      string `json:"tag"`
-	Value    string `json:"value"`
-	Message  string `json:"message"`
+	Message string `json:"message"`
 }
 
-func GetValidationErrors(err error) *[]ValidationError {
-	var validationErrors []ValidationError
+var fieldTranslations = map[string]string{
+	"FullName":    "نام کامل",
+	"AccountType": "نوع حساب",
+	"SaleCount":   "تعداد فروش",
+	"Mobile":      "شماره موبایل",
+	"Password":    "رمز عبور",
+}
+
+func translateField(fieldName string) string {
+	if translation, ok := fieldTranslations[fieldName]; ok {
+		return translation
+	}
+	return fieldName
+}
+
+func TranslateValidationError(tag string) string {
+	switch tag {
+	case "required":
+		return "اجباری است"
+	case "min":
+		return "کمتر از حد مجاز است"
+	case "max":
+		return "بیشتر از حد مجاز است"
+	case "len":
+		return "باید دقیقاً طول مشخص شده داشته باشد"
+	default:
+		return "خطا در اعتبارسنجی"
+	}
+}
+
+func GetValidationErrors(err error) []*string {
+	var validationErrors []*string
 	var ve validator.ValidationErrors
 	if errors.As(err, &ve) {
 		for _, err := range err.(validator.ValidationErrors) {
-			var el ValidationError
-			el.Property = err.Field()
-			el.Tag = err.Tag()
-			el.Value = err.Param()
-			validationErrors = append(validationErrors, el)
+			errorMessage := translateField(err.Field()) + " " + TranslateValidationError(err.Tag())
+			validationErrors = append(validationErrors, &errorMessage)
 		}
-		return &validationErrors
+		return validationErrors
 	}
 	return nil
 }

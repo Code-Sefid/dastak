ee94776f67f3ce79380a4be5e649c2a2144fc638
soheil <soheilkhaledabdi@gmail.com>
2023-09-30 14:01:17 +0330

    Bug -> fix bug

--- api/dto/bank.go
+++ api/dto/bank.go
@@ -1,14 +1,14 @@
 package dto
 
 type CreateBankRequest struct {
-	CardName    string `json:"cardName" binding:"required,min=12,max=12"`
-	CardNumber  string `json:"cardNumber" binding:"required,min=3"`
+	CardName    string `json:"cardName" binding:"required,min=3"`
+	CardNumber  string `json:"cardNumber" binding:"required,min=12,max=12"`
 	ShabaNumber string `json:"shabaNumber"`
 }
 
 type UpdateBankRequest struct {
-	CardName    string `json:"cardName" binding:"min=12,max=12"`
-	CardNumber  string `json:"cardNumber" binding:"min=3"`
+	CardName    string `json:"cardName" binding:"min=3"`
+	CardNumber  string `json:"cardNumber" binding:"min=12,max=12"`
 	ShabaNumber string `json:"shabaNumber"`
 }
 

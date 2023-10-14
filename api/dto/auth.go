295cc61d4f932784b4f1eaf5372993273dd7ba64
soheil <soheilkhaledabdi@gmail.com>
2023-10-14 14:25:13 +0330

    Update -> add dto and validation

--- api/dto/auth.go
+++ api/dto/auth.go
@@ -5,16 +5,16 @@ type Register struct {
 	FullName    string `json:"full_name" binding:"required,min=3" `
 	AccountType int    `json:"account_type" binding:"required" `
 	SaleCount   uint   `json:"sale_count" binding:"required" `
-	Mobile      string `json:"mobile" binding:"required,min=11,max=11" `
+	Mobile      string `json:"mobile" binding:"required,min=11,max=11,mobile" `
 }
 
 // Login struct
 type Login struct {
-	Mobile   string `json:"mobile" binding:"required,min=11,max=11" `
+	Mobile   string `json:"mobile" binding:"required,min=11,max=11,mobile" `
 	Password string `json:"password" binding:"required,min=4" `
 }
 
 // Mobile struct
 type Mobile struct {
-	Mobile string `json:"mobile" binding:"required,min=11,max=11" `
+	Mobile string `json:"mobile" binding:"required,min=11,max=11,mobile" `
 }

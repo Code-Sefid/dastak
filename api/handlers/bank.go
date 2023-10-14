101f6facb2e9c8cf0d2ec8c696c252e2e3215a24
soheil <soheilkhaledabdi@gmail.com>
2023-10-14 16:54:12 +0330

    Update -> edit code

--- api/handlers/bank.go
+++ api/handlers/bank.go
@@ -1,8 +1,13 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/soheilkhaledabdi/dastak/api/dto"
+	"github.com/soheilkhaledabdi/dastak/api/helper"
 	"github.com/soheilkhaledabdi/dastak/config"
+	"github.com/soheilkhaledabdi/dastak/constants"
 	"github.com/soheilkhaledabdi/dastak/services"
 )
 
@@ -17,7 +22,23 @@ func NewBankHandler(cfg *config.Config) *BankHandler {
 }
 
 func (h *BankHandler) Create(c *gin.Context) {
-	CreateByUserId(c, h.service.CreateByUserId)
+	userID := int(c.Value(constants.UserIdKey).(float64))
+	req := new(dto.CreateBankRequest)
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest,
+			helper.GenerateBaseResponseWithValidationError(false, err, "لطفا اطلاعات ها را به درستی پر کنید"))
+		return
+	}
+	res, err := h.service.CreateOrUpdate(c, req, userID)
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError,
+			helper.GenerateBaseResponseWithError(false, err, "لطفا دوباره مجدد امتحان بکنید یا با پشتیبانی ارتباط بگیرید"))
+		return
+	}
+
+	c.JSON(http.StatusOK, helper.GenerateBaseResponse(res, true, ""))
 }
 
 func (h *BankHandler) Update(c *gin.Context) {

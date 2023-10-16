0c097e05e309a9fbc754f599a074bf72a5b048bc
soheil <soheilkhaledabdi@gmail.com>
2023-10-16 10:30:25 +0330

    Add -> add new services for transactions

---
+++ services/transations_services.go
@@ -0,0 +1,71 @@
+package services
+
+import (
+	"context"
+	"errors"
+
+	"github.com/soheilkhaledabdi/dastak/api/dto"
+	"github.com/soheilkhaledabdi/dastak/config"
+	"github.com/soheilkhaledabdi/dastak/data/db"
+	"github.com/soheilkhaledabdi/dastak/data/models"
+	"github.com/soheilkhaledabdi/dastak/pkg/logging"
+	"gorm.io/gorm"
+)
+
+type TransactionsService struct {
+	logger       logging.Logger
+	cfg          *config.Config
+	tokenService *TokenService
+	database     *gorm.DB
+}
+
+func NewTransactionsService(cfg *config.Config) *TransactionsService {
+	database := db.GetDb()
+	logger := logging.NewLogger(cfg)
+	return &TransactionsService{
+		cfg:          cfg,
+		database:     database,
+		logger:       logger,
+		tokenService: NewTokenService(cfg),
+	}
+}
+
+
+
+func (s *TransactionsService) GetByFilter(ctx context.Context, userId int) ([]*dto.TransactionsResponse, error) {
+	tx := s.database.WithContext(ctx).Begin()
+	defer func() {
+        if r := recover(); r != nil {
+            tx.Rollback()
+        } else {
+            tx.Commit()
+        }
+    }()
+
+	var transaction []*models.Transactions
+
+	err := tx.Where("user_id = ?",userId).Find(&transaction).Error
+	if err != nil && errors.Is(err,gorm.ErrRecordNotFound) {
+		return nil, err
+	}
+	
+	responses := make([]*dto.TransactionsResponse,len(transaction))
+	for _,item := range transaction{
+		var title string
+
+		if item.TransactionType == models.SALES  {
+			title = "پرداخت مشتری"
+		}else if item.TransactionType == models.WITHDRAW {
+			title = "برداشت از کیف پول"
+		}else {
+			title = "سود همکاری"
+		}
+		responses = append(responses, &dto.TransactionsResponse{
+			Title: title,
+			Message: item.Description,
+			Amount: item.Amount,
+		})
+	}
+	return responses,nil	
+
+}

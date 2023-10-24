5bc45a0b04e99b92f8866ecb8603d40d85256122
soheil <soheilkhaledabdi@gmail.com>
2023-10-24 21:04:27 +0330

    Add -> UsersAssist to users

--- data/models/users.go
+++ data/models/users.go
@@ -1,18 +1,23 @@
 package models
 
 type UsersType string
+type UsersAssist string
 
 const (
 	DIGITAL  UsersType = "digital"
 	PHYSICAL UsersType = "physical"
+
+	NORMAL  UsersAssist = "normal"
+	PARTNER UsersAssist = "partner"
 )
 
 type Users struct {
 	BaseModel
 	FullName       string `gorm:"type:VARCHAR(255)"`
 	Type           UsersType
-	Mobile         string `gorm:"type:VARCHAR(255)"`
-	Password       string `gorm:"type:VARCHAR(255)"`
-	ReferralMobile string `gorm:"type:VARCHAR(255)"`
+	Assist         UsersAssist `gorm:"type:VARCHAR(255)"`
+	Mobile         string      `gorm:"type:VARCHAR(255)"`
+	Password       string      `gorm:"type:VARCHAR(255)"`
+	ReferralMobile string      `gorm:"type:VARCHAR(255)"`
 	SaleCount      uint
 }
